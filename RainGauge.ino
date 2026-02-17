e62ac258d65cb33080efa7a6e94ae8e942bbbb76
RichardGould <richard.gould@ntlworld.com>
2026-02-17 16:44:51 +0000

    Add files via upload
    Latest version with verbose output to MQTT

--- RainGauge.ino
+++ RainGauge.ino
@@ -2,35 +2,35 @@
 //    MiS Rain Gauge
 
 #include <HX711.h>
-//#include <Servo.h>
-#include <ESP8266_ISR_Servo.h>
+#include <Servo.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
-#include <ESP8266mDNS.h>
-#include <ArduinoOTA.h>
+//#include <ESP8266mDNS.h>
+//#include <ArduinoOTA.h>
 #include <WiFiUdp.h>
-#include <BMP280.h>
+#include <PTSolns_AHTx.h>
+#include <BMx280.h>
 
-#define MY_PIN D7
-#define MY_MIN 800
+#define MY_PIN  D5
+#define MY_MIN 540
 #define MY_MAX 2450
 #define MY_MID 1500
 #define MY_DEL 15
-#define MY_HOME 130
-#define MY_MOUT 20
-#define MY_SDA D1
-#define MY_SCL D2
-#define MY_HXD D6
-#define MY_HXC D5
+#define MY_HOME 150
+#define MY_MOUT 40
+#define MY_SDA D2
+#define MY_SCL D1
+#define MY_HXD D7
+#define MY_HXC D6
 #define MSG_BUFFER_SIZE 50
 #define MQTT_SIZE 20
-#define TWEIGHT 25
-#define PERIOD 10
+#define TWEIGHT 5
+#define PERIOD 30
 
 #define SizePass 20
 
-//Servo myservo;  // create servo object to control a servo
+Servo myservo;  // create servo object to control a servo
 
 /*  variable instantiation  */
 
@@ -62,7 +62,7 @@ const char* Version       = "0.1";
 
 char MQTT_server[20];
 char MQTT_PUB[200], MQTT_IN[MQTT_SIZE], MQTT_OUT[MQTT_SIZE], MQTT_RST[MQTT_SIZE], MQTT_STATIN[MQTT_SIZE], MQTT_STATOUT[ MQTT_SIZE], MiS_HEAD[10];
-char MQTT_TIME[20];
+char MQTT_TIME[20], MQTT_TIP[20];
 
 int MQTT_PORT = 1883;
 char my_dir[ 10 ];
@@ -71,10 +71,10 @@ char PUB_message[MSG_BUFFER_SIZE], SUB_message[MSG_BUFFER_SIZE];
 char MQTT_in_buffer[MSG_BUFFER_SIZE], MQTT_in_topic[20];
 char my_IP_Address[20];  //  xxx:qsort.xxx.xxx.xxx
 int MQTT_in_flag, MQTT_in_length;
-unsigned long epoch = 0, now = 0, interval = 0, pulse, payload_time;
+unsigned long epoch = 0, now = 0, interval = 0, pulse, payload_time, period;
 uint8_t i = 0, rtn = 0, count = 0;
 int net, network, networks;
-int in, then, pa, period;
+int in, then, pa;
 char * q;
 /*  time variables */
 char a_timestamp[ 13 ], a_year[ 5 ], a_month[ 4 ], a_day[ 4 ], a_hour[ 4 ], a_minute[ 4 ], a_second[ 4 ];
@@ -87,10 +87,21 @@ int time_flag = 0;
 int ON  = 1;
 int OFF = 0;
 
-int sequence = 0, tips = 0;
+int sequence = 0, tips = 0, time_seq = 0;
 uint8_t dataPin  = MY_HXD;
 uint8_t clockPin = MY_HXC;
-float weight = 0, oweight = 0;
+float weight = 0, oweight = 0, start_weight, temp;
+
+// Setting PWM properties
+const int APWM = MY_PIN;
+const int freq = 100000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int dutyCycle = 200;
+
+const uint8_t  I2C_ADDR      = 0x76;     // Default address
+const unsigned POLL_MS       = 2000;     // [mS] How often to read sensor
+const float    SEA_LEVEL_HPA = 1013.25f;
 
 /*  Instance creation */
 
@@ -100,7 +111,10 @@ PubSubClient client(espClient);    // MQTT instance
 
 HX711 scale;                        // HX711 instance
 
-BMP280 bmp;
+PTSolns_AHTx aht;
+
+BMx280 bmx;
+
 /*
  *	Setup
  */
@@ -108,7 +122,18 @@ void setup()
 {
   Serial.begin(115200);
   while (!Serial) delay(1000);
+  Wire.begin( D2, D1 );
+  if (!bmx.beginI2C(I2C_ADDR)) { 
+    Serial.println("BMx280 not found"); 
+    while(1){
+      Serial.println("BMx280 not found"); 
+    } 
+  }
+  bmx.setSampling(1,1,1, 0,0, BMx280::MODE_NORMAL); 
 
+  Serial.print("chipID=0x"); Serial.println(bmx.chipID(), HEX);
+  Serial.print("hasHumidity="); Serial.println(bmx.hasHumidity());
+  Serial.println("I2C Continuous");
 /*
  *    Construct MQTT tokens
  */ 
@@ -119,6 +144,7 @@ void setup()
   sprintf( MQTT_STATIN,   "%s/%s", MiS_HEAD, STAT_IN_TOPIC );
   sprintf( MQTT_STATOUT,  "%s/%s", MiS_HEAD, STAT_OUT_TOPIC );
   sprintf( MQTT_TIME,     "%s/%s", MiS_BASE, "TIME");
+  sprintf( MQTT_TIP,      "%s/%s", MiS_HEAD, "TIP");
 /*
  *  decrypt passwords
  */
@@ -157,73 +183,39 @@ void setup()
   client.loop();
   fn_MQTT_Connect();
   Serial.println(" MQTT Connected ");
-/*
- *	Arduino OTA Setup
- */ 
-  Serial.println(" OTA Connecting ");
-  ArduinoOTA.setHostname("newvane");
-  ArduinoOTA.setPassword("starwest");
-/*
- *  Arduino OTA Callbacks
- */
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else {  // U_FS
-      type = "filesystem";
-    }
-  });
-  
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR)   Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR)     Serial.println("End Failed");
-	});
-
-  ArduinoOTA.begin();
-
-  Serial.println(" OTA Connected ");
-
+ 
   epoch = millis();      //  start the clock
 
-  servo = ISR_Servo.setupServo(MY_PIN, MY_MIN,  MY_MAX);
-  if (servo != -1) {
-    Serial.println(F("Setup Servo1 OK"));
-   } else {
-    Serial.println(F("Setup Servo1 failed"));
-  }
-  //myservo.attach( MY_PIN, MY_MIN, MY_MAX, MY_MID );
-
+  Serial.println("1");
   scale.begin(MY_HXD, MY_HXC);
+  Serial.println("2");
+  //scale.set_gain( 128 );
 
   if ( !scale.is_ready() ) delay(1000);
+  Serial.println("3");
+  scale.tare(20);
+  Serial.println("4");
+  int32_t offset = scale.get_offset();
 
-  scale.set_scale( -9000 );
-  scale.set_offset(-110000);
+  Serial.print("OFFSET: ");
+  Serial.println(offset);
+  Serial.println();
+
+  scale.set_scale( 10600 );
+  scale.set_offset(870000);
 
   //  reset the scale to zero = 0
-  scale.tare(10);
+  scale.tare(0);
 
-  //myservo.write( MY_HOME );
-  ISR_Servo.setPosition(servo, MY_HOME);
+	myservo.attach(MY_PIN, 700, 2300);
 
-  //analogWriteFreq( 200 );
-  Wire.begin( D1, D2 );
+  Wire.begin( D2, D1 );
 
-  bmp.begin();
+  aht.begin();
+
+  myservo.write(MY_HOME);
+
+  time_seq = PERIOD * 1000;
 
   Serial.println(" Setup ends ");
 }
@@ -233,13 +225,20 @@ void fn_tip( void )
   int pos = 0;
   for ( pos = MY_HOME; pos >= MY_MOUT; pos-- )
   {
-    ISR_Servo.setPosition(servo, pos);
- //   myservo.write( pos );
-    delay( 10 );
+    myservo.write( pos );
+	  delay( 10 );
   }
   delay( 2000 );
-  ISR_Servo.setPosition(servo, MY_HOME);
-//  myservo.write( MY_HOME );
+  for ( pos = MY_MOUT; pos <= MY_HOME; pos++ )
+  {
+    myservo.write( pos );
+	  delay( 20 );
+  }
+  delay( 2000 );
+
+//  myservo.write( MY_HOME);
+  scale.tare(0);
+  start_weight = 0;
   tips++;
 }
 
@@ -365,9 +364,15 @@ int MQTT_CB(char* topic, byte* payload, uint8_t length) {
       client.publish(MQTT_STATOUT, (const uint8_t*)MQTT_PUB, strlen(MQTT_PUB), false);
     }
   }
+
   if (strcmp(MQTT_in_topic, MQTT_RST) == 0)
   {
     if (strcmp(MQTT_in_buffer, "Yes") == 0) fn_ReStart();
+  }
+
+  if (strcmp(MQTT_in_topic, MQTT_TIP) == 0)
+  {
+    if (strcmp(MQTT_in_buffer, "Yes") == 0) fn_tip();
   }
   return (1);
 }
@@ -381,6 +386,7 @@ int fn_MQTT_Connect() {
       client.subscribe(MQTT_RST,  1);
       client.subscribe(MQTT_STATIN, 1);
       client.subscribe(MQTT_TIME, 1);
+      client.subscribe(MQTT_TIP, 1);
       return (0);
     }
     Serial.print(F("."));
@@ -395,49 +401,66 @@ void fn_ReStart(void) {
   ESP.restart();
 }
 
+void fn_delay( int interval )
+{
+  now = millis();
+  while ( ( millis() - now ) < interval )
+  {
+    delay( 1000 );
+      client.loop();
+  }
+}
+
 void loop()
 {
   epoch = millis();
   client.loop();
-  float temp = bmp.getTemperature();
-
-  ArduinoOTA.handle();
+  float temp, humid;
+  float T, P_hPa, H;
+  AHTxStatus st = aht.readTemperatureHumidity(temp, humid, 120);
+  bmx.read280(T, P_hPa, H);
+ 
   sequence++;
   weight = 0;
   if (scale.is_ready())
   {
     weight = scale.get_units(10);
+    if ( start_weight == 0 ) start_weight = weight;
    // float readw =   scale.read_average( 10 );
     if ( weight < 0 ) weight = 0 - weight;
-    if ( weight > ( TWEIGHT + 10 ) ) weight = oweight;
-    sprintf( MQTT_PUB, "$%05i,%s,%02i:%02i:%02i,%05i,%05i,%07.2f,%07.2f#", sequence, a_date, hour, minute, second, epoch - now, tips, temp, weight );
+    if ( weight > ( TWEIGHT + TWEIGHT ) ) weight = oweight;
 
+    sprintf( MQTT_PUB, "$%05i,%s,%02i:%02i:%02i,%05i,%05i,%07.2f,%07.2f,%07.2f,%07.2f,%0.2f#",
+           sequence, a_date, hour, minute, second, epoch - now, tips, temp, humid, T, P_hPa,  weight );
+    Serial.println( MQTT_PUB );
     if (!client.connected()) fn_MQTT_Connect();
     client.publish(MQTT_OUT, (const uint8_t*)MQTT_PUB, strlen(MQTT_PUB), false);
-    Serial.println( MQTT_PUB);
 
-    if ( weight >= TWEIGHT ) fn_tip();
+   // fn_tip();
+
+    if ( weight >= ( start_weight + TWEIGHT ) ) fn_tip();
     oweight = weight;
   }
   interval = millis() - epoch;
 
   second = second + PERIOD;
   if ( second >= 60 ) {
-    minute++;
+    minute = minute + 1;
     second = second - 60;
     if ( minute >= 60 ) {
-      hour++;
+      hour = hour + 1;
       minute = minute - 60;
       if ( hour >= 24 ) hour = hour - 24; 
     }
   }
-  if ( hour == 0 ) {
+  if ( hour == 1 ) {
     if ( minute == 5 ) {
         client.subscribe( MQTT_TIME );
     }
   }
   now = epoch;
-  delay( ( PERIOD * 1000 ) - interval );
+  if ( interval > ( time_seq ) ) interval = ( time_seq ) -100;
+  fn_delay( ( time_seq ) - interval );
 }
 //  -- END OF FILE --
 
